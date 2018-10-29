5b53e1e85ea60d505127dac72edd857bb0c998d4
Michał Bachta <michalbachta@gmail.com>
2018-10-29 10:49:06 +0100

    Alphabet letters bar & it's item. MainMemuListItem fix.

--- src/views/alphabet/alphabetLettersBar/AlphabetLettersBarListItem.vue
+++ src/views/alphabet/alphabetLettersBar/AlphabetLettersBarListItem.vue
@@ -1,15 +1,38 @@
 <template>
-<div>
-
-</div>
+<li class="alphabet__letters-bar-list-item noselect"
+>{{ letter }}</li>
 </template>
 
 <script>
 export default {
-    name: 'AlphabetLettersBarListItem'
+    name: 'AlphabetLettersBarListItem',
+    props: {
+        letter: {
+            // ToDo: uncomment when get real letters
+            // type: String,
+            required: true
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../../styles/dimens';
+@import '../../../styles/colors';
 
+.alphabet__letters-bar-list-item {
+    width: $appBarSize;
+    height: $appBarSize;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    font-weight: bolder;
+    transform: rotate(90deg);
+    color: white;
+    cursor: pointer;
+    &:hover {
+        background: $itemHoverBackGround;
+    }
+}
 </style>
